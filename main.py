7aadee629294dc95270dbde75d02056dc119e9e8
Alexander Dickbauer <alexander.dickbauer@nowconsultians.at>
2023-02-21 21:18:17 +0100

    Added Main script for handling slurm cluster jobs in the background

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
-import torch
-from AlphaZero.AlphaHex import AlphaHex
-from Enviorment.hex_engine_0_5 import hexPosition as HexGame
-from AlphaZero.Player import DeepLearningPlayer
+import subprocess
+import time
+
+result = subprocess.run(['squeue'], stdout=subprocess.PIPE)
+n = 0
+commands = {
+    1: ["sbatch","1_generator_slurm.sh"],
+    2: ["sbatch","2_concate_slurm.sh"],
+    3: ["sbatch","3_train_slurm.sh"],
+    4: ["sbatch","4_evaluate_slurm.sh"],
+    5: ["sbatch","5_modelselect_slurm.sh"],
+}
+counter = 1
+free = True
+while n < 50:
+    if free:
+        subprocess.run(commands[counter])
+        free = False
+
+    time.sleep(10)
+    result = subprocess.run(['squeue'], stdout=subprocess.PIPE)
+    result = str(result).replace("main_slu ai21m034", "")
+    if "ai21m034" not in str(result):
+        free = True
+        counter += 1
+        if counter > 5:
+            n += 1
+            counter = 1
 
 
-x = torch.randn(1, 1, 8, 8)
-#g = HexGame(8)
-#print(g.board)
-model = AlphaHex(board_size=8)
-
-if __name__ == "__main__":
-    g = HexGame(8)
-    player1 = DeepLearningPlayer(model, rollouts=400, competitive=True)
-    player2 = DeepLearningPlayer(model, rollouts=400)
-    print(player1.getMove(g))
-    print(g.board)
-    #g.makeMove()
-
-    # player2 = DeepLearningPlayer(current_model)
-    #game, new_training_data = play_game(g, player1, player2, False)
-    #training_data += new_training_data
 
 
 
